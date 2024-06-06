cfab0a51754c5a6a6e3e889aeb9261c02c92c456
Varun <varunrawat343@gmail.com>
2024-06-06 23:09:43 +0530

    Integrate dotenv

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 import express from "express";
 import userRouter from "./routes/user.js";
+import { config } from "dotenv";
 
-const app = express();
-const PORT = 4000;
+export const app = express();
+
+config({
+  path: "./data/config.env",
+});
 
 app.use(express.json());
 app.use("/users", userRouter);
 
 app.get("/", (req, res) => {
   res.send("WoW");
-});
-
-app.listen(PORT, () => {
-  console.log(`Server running on: ${PORT}`);
 });
