dcbae33bb949cc726833bd65732d0e28b3663ab9
Varun <varunrawat343@gmail.com>
2024-06-06 23:47:35 +0530

    Initial project setup

--- data/database.js
+++ data/database.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connectDB = () => {
   mongoose
     .connect(process.env.MONGO_URI, {
-      dbName: "backend_api",
+      dbName: "toDoApp",
     })
     .then(() => console.log("Database Connected"))
     .catch((e) => console.log(e));
