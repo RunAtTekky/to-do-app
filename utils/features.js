81037fe76c4e6a370615b929f838cecf24a67dff
Varun <varunrawat343@gmail.com>
2024-06-08 21:58:37 +0530

    set sameSite property none

--- utils/features.js
+++ utils/features.js
@@ -7,7 +7,7 @@ export const sendCookie = (user, res, message, statusCode = 200) => {
     .cookie("token", token, {
       httpOnly: true,
       maxAge: 15 * 60 * 1000,
-      sameSite: "None",
+      sameSite: "none",
       secure: true,
     })
     .json({
